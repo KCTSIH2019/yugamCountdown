a70d96dff4395aa9b1bb6bdb4718b8a974d99546
iQube KI <48145876+KCTSIH2019@users.noreply.github.com>
2025-01-17 10:20:39 +0530

    Update script.js
    year change

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ $(document).ready(function(e) {
         $('#ytvid').tubular(options);
     }
     // Set the date we're counting down to
-    var countDownDate = new Date("Mar 22, 2024 00:00:00").getTime();
+    var countDownDate = new Date("Mar 06, 2025 00:00:00").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function() {
